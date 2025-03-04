50c141f55d9618351d63a3ca1be53658b0ce338c
Md Hridoy Sheikh <mdhridoy3240@gmail.com>
2025-03-04 23:38:52 +0600

    complete website

--- js/body.js
+++ js/body.js
@@ -20,3 +20,80 @@ const formattedDate = `${dayOfWeek}, ${month} ${day} ${year}`;
 document.addEventListener("DOMContentLoaded", function() {
     document.getElementById('date').innerText = formattedDate;
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let numbersix= document.getElementById("number-minus");
+const plusnum = document.getElementById('numberplus');
+const clickCompleted = document.getElementsByClassName('click-completed');
+
+for (let i = 0; i < clickCompleted.length; i++) {
+    clickCompleted[i].addEventListener('click', function(event) {
+        alert("Board update Successfully")
+        const tittle = event.target.parentElement.parentElement.parentElement.getElementsByClassName('title')[0].innerText;
+        event.target.setAttribute('disabled', true);
+        // event.target.classList.add( 'bg-[#ced6ff]')
+        event.target.style.backgroundColor='#ced6ff';
+        
+        const numberSixVert = parseInt(numbersix.innerText);
+        numbersix.innerText= numberSixVert-1;
+
+
+       const plusnumVert = parseInt(plusnum.innerText);
+       plusnum.innerText = plusnumVert+1;
+
+
+       const now = new Date();
+       let hours = now.getHours();
+       const minutes = now.getMinutes().toString().padStart(2, '0');
+       const seconds = now.getSeconds().toString().padStart(2, '0');
+       const ampm = hours >= 12 ? 'PM' : 'AM';
+       
+       
+       hours = hours % 12 || 12;
+       
+       const currentTime = `${hours}:${minutes}:${seconds} ${ampm}`;
+       
+       
+        const newContent = `
+       <p class="mb-5 p-2 bg-[#F4F7FF] font-normal">
+          You have Complete The Task ${tittle} at ${currentTime}
+       </p>
+       `;
+
+
+
+     
+
+      document.getElementById('addtag').innerHTML += newContent;
+      
+
+
+        if(numberSixVert === 1){
+            alert("hridoy")
+        }
+    });
+}
+
+
+
+const clearData = document.getElementsByClassName('clear-borard')[0];
+const clearDatas = document.getElementById('addtag')
+clearData.addEventListener('click', function(event) {
+    clearDatas.innerHTML = "";
+    
+   
+})
